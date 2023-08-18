5202e41c918d9f3aff0116dd303ae47d0e007fbc
Shobande James Oluwaseun <shobandejames@gmail.com>
2023-08-18 23:47:16 +0100

    lot of fixes, url changes

--- src/components/OrderList.jsx
+++ src/components/OrderList.jsx
@@ -1,118 +1,225 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import {  toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const OrderList = () => {
-
-    const notify = (toastType, message) => toastType(message, {
-        position: "top-center",
-        theme: "colored",
-        autoClose: 2000,
+  const notify = (toastType, message) =>
+    toastType(message, {
+      position: "top-center",
+      theme: "colored",
+      autoClose: 2000,
     });
 
-    const [items, setItems] = useState([])
+  const baseURL = process.env.REACT_APP_BASE_URL;
 
-    const [newStatus, setNewStatus] = useState("")
+  const [name, setName] = useState("Seun");
 
-   const  handleChange = (event, rowIndex) => {
-        const selectedValue = event.target.value;
-        setNewStatus(selectedValue)
-        // console.log(`Selected value for row ${rowIndex}: ${selectedValue}`);
-      };
+  const [modalClose, setModalClose] = useState(false);
 
-      const  handleSubmit = ( rowIndex) => {
-        // console.log(`Selected value for row ${rowIndex}: ${newStatus}`);
+  const [refreshCount, setRefreshCount] = useState(0);
 
-        const requestbody = {
-            status: newStatus
-        }
+  const [isLoading, setIsLoading] = useState(false);
 
-        const requestOptions = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(requestbody)
-        };
+  const [items, setItems] = useState([]);
 
-        fetch(`https://localhost:7248/api/Order/updateorderstatus?query=${rowIndex}`, requestOptions)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error(response.status);
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (!data.success) {
-                    // console.log(data.message)
-                }
-                notify(toast.success, "Order Updated Successfully")
-            })
-            .catch((error) => {
-                // console.log(error.message);
-                notify(toast.error, "Error Updating Order")
-                //if (error.message === 401) {
-                //    console.log("Not Authorized")
-                //} else if (error.message == 404) {
-                //    console.log("Not Found")
-                //}
-            });
+  const [newStatus, setNewStatus] = useState("");
 
-        setNewStatus("")
-      };
+  const handleChange = (event, rowIndex) => {
+    const selectedValue = event.target.value;
+    setNewStatus(selectedValue);
+  };
 
+  const handleDelete = async (rowIndex) => {
+    setIsLoading(true);
 
-    useEffect(() => {
-        axios.get("https://localhost:7248/api/Order/getallorders", {
-        })
-            .then((res) => {
-                var data = res.data
-                setItems(data.data);
-            })
-            .catch((error) => {
-                console.error(error)
-            })
-    }, [])
-
-    return (
-        <div>
-            <h3> Order List </h3>
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>S/N</th>
-                        <th>Order ID </th>
-                        <th> Tracking ID  </th>
-                        <th> Status </th>
-                        <th>Update Status</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    {items.length > 0 ? items.map((item, index) =>
-                        <tr key={item.id}>
-                            <td>{index + 1}</td>
-                            <td>{item.orderId}</td>
-                            <td>{item.trackingId}</td>
-                            <td>{item.status}</td>
-                            <td>
-                                <select className="form-select form-select-sm" aria-label=".form-select-sm example"  onChange={(event) => handleChange(event, item.trackingId)}>
-                                    <option selected > {item.status} </option>
-                                <option value="Order confirmed">Order Confirmed </option>
-                                <option value="Picked by courier">Picked by Courier</option>
-                                <option value="On the way"> On the way </option>
-                                <option value="Ready for pickup">Ready for pickup</option>
-                                </select>
-                            </td>
-                            <td><button disabled = {newStatus.length === 0 } btn btn-success onClick={() => handleSubmit( item.trackingId)}>Update </button></td>
-                        </tr>
-                    ) : <td className="py-3"> No Order yet </td>}
-                </tbody>
-            </table>
-        </div>
-
+    fetch(
+      `http://localhost:3900/api/order/deleteorderbytrackingid?trackingId=${rowIndex}`
     )
-}
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        // Refresh component after successful POST
+        setRefreshCount(refreshCount + 1);
+        notify(toast.success, "Order Deleted Successfully");
+        return response.json();
+      })
+      .catch((error) => {
+        notify(toast.error, "Error Updating Order");
+      });
+    setModalClose(true);
+    setIsLoading(false);
+  };
 
-export default OrderList
+  const handleSubmit = (rowIndex) => {
+    // console.log(`Selected value for row ${rowIndex}: ${newStatus}`);
+    setIsLoading(true);
+
+    const requestbody = {
+      status: newStatus,
+    };
+
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestbody),
+    };
+
+    fetch(
+      `http://localhost:3900/api/order/updateorderstatus?orderId=${rowIndex}`,
+      requestOptions
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(response.status);
+        }
+        // Refresh component after successful POST
+        setRefreshCount(refreshCount + 1);
+        notify(toast.success, "Order Updated Successfully");
+        return response.json();
+      })
+      .catch((error) => {
+        notify(toast.error, "Error Updating Order");
+      });
+
+    setIsLoading(false);
+    setNewStatus("");
+  };
+
+  const getOrder = async () => {
+    setIsLoading(true);
+    axios
+      .get(`${baseURL}/order/getorders`, {})
+      .then((res) => {
+        var data = res.data;
+        setItems(data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    if (refreshCount >= 0) {
+      getOrder();
+    }
+  }, [refreshCount]);
+
+  return (
+    <div className="table-responsive">
+      <h3> Order List </h3>
+      {items.length > 0 ? (
+        <table className="table table-striped" aria-labelledby="tabelLabel">
+          <thead>
+            <tr>
+              <th>S/N</th>
+              <th>Order ID </th>
+              <th> Tracking ID </th>
+              <th> Status </th>
+              <th>Update Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            {items.map((item, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{item.orderId}</td>
+                <td>{item.trackingId}</td>
+                <td>{item.status}</td>
+                <td>
+                  <select
+                    className="form-select form-select-sm"
+                    aria-label=".form-select-sm example"
+                    onChange={(event) => handleChange(event, item.orderId)}
+                  >
+                    {/* <option selected> {item.status} </option> */}
+                    <option value="Order Confirmed">Order Confirmed</option>
+                    <option value="Picked By Courier">Picked by Courier</option>
+                    <option value="On The Way">On The Way</option>
+                    <option value="Ready For Pickup">Ready For Pickup</option>
+                  </select>
+                </td>
+                <td>
+                  <button
+                    disabled={newStatus.length === 0}
+                    className="btn btn-success"
+                    onClick={() => handleSubmit(item.orderId)}
+                  >
+                    Update{" "}
+                  </button>
+                </td>
+                <td>
+                  <button
+                    type="button"
+                    className="btn btn-danger"
+                    data-bs-toggle="modal"
+                    data-bs-target="#exampleModal"
+                    onClick={() => setName(item.trackingId)}
+                  >
+                    <span className="icon">
+                      {" "}
+                      <i className="fa fa-trash"></i>{" "}
+                    </span>
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <h4 className="text-center"> No orders yet </h4>
+      )}
+
+      {/* Modal */}
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Delete?
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div className="modal-body">
+              Are you sure you want to delete order {name} ?
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button
+                type="button"
+                className="btn btn-primary"
+                data-bs-dismiss="modal"
+                onClick={() => handleDelete(name)}
+              >
+                Ok
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OrderList;
--- src/components/TrackingDetails.jsx
+++ src/components/TrackingDetails.jsx
@@ -1,50 +1,131 @@
-import React from 'react'
-import item from "../assets/item.png"
-import { Link } from 'react-router-dom';
+import React from "react";
+import item from "../assets/item.png";
+import { Link } from "react-router-dom";
 
-const TrackingDetails = ({order}) => {
-
+const TrackingDetails = ({ order }) => {
   return (
-    <div className='card'>
-                <header className="card-header"> My Orders / Tracking </header>
-                <div className="card-body">
-                    <h6>Order ID: {order.orderId}  </h6>
-                    <div className="card ">
-                        <div className="card-body row">
-                            <div className="col"> <strong>Estimated Delivery time:</strong> <br /> {order.deliveryDate.slice(0, -14)} </div>
-                            <div className="col"> <strong>Shipping BY:</strong> <br /> {order.shippedBy} </div>
-                            <div className="col"> <strong>Status:</strong> <br /> {order.status} </div>
-                            <div className="col"> <strong>Tracking #:</strong> <br /> {order.trackingId} </div>
-                        </div>
-                    </div>
-                    <div className="track">
-                        <div className={`step ${order.status.includes("Order") ||order.status.includes("Picked") || order.status.includes("On") || order.status.includes("Ready") ? "active" : null }`}> <span className="icon"> <i className="fa fa-check"></i> </span> <span className="text">Order confirmed</span> </div>
-                        <div className={`step ${order.status.includes("Picked") || order.status.includes("On") || order.status.includes("Ready") ? "active" : null }`} > <span className="icon"> <i className="fa fa-user"></i> </span> <span className="text"> Picked by courier</span> </div>
-                        <div className={`step ${order.status.includes("On") || order.status.includes("Ready") ? "active" : null }`}> <span className="icon"> <i className="fa fa-truck"></i> </span> <span className="text"> On the way </span> </div>
-                        <div className={`step ${order.status.includes("Ready") ? "active" : null }`} > <span className="icon"> <i className="fa fa-box"></i> </span> <span className="text">Ready for pickup</span> </div>
-                    </div>
-
-                    <ul className="row  py-5">
-                    {order.items.map((orderItem, index) =>
-                         <li className="col-md-4">
-                         <figure className="itemside mb-3">
-                             <div className="aside"><img src={item} className="img-sm border" alt='' /></div>
-                             <figcaption className="info align-self-center">
-                                 <p className="title">{orderItem.name} <br /> </p> <span className="text-muted">${orderItem.price} </span>
-                             </figcaption>
-                         </figure>
-                     </li>
-                     )}
-                        
-                    </ul>
-
-                    <hr />
-                    <Link to="/" className="btn btn-warning maroon-bg text-dark" data-abc="true"> <i className="fa fa-chevron-left"></i> Back to Home</Link>
-
-                </div>
-
+    <div className="card">
+      <header className="card-header"> My Orders / Tracking </header>
+      <div className="card-body">
+        <h6>Order ID: {order.orderId} </h6>
+        <div className="card ">
+          <div className="card-body row">
+            <div className="col">
+              {" "}
+              <strong>Estimated Delivery time:</strong> <br />{" "}
+              {order.deliveryDate.slice(0, -14)}{" "}
             </div>
-  )
-}
+            <div className="col">
+              {" "}
+              <strong>Shipping BY:</strong> <br /> {order.shippedBy}{" "}
+            </div>
+            <div className="col">
+              {" "}
+              <strong>Status:</strong> <br /> {order.status}{" "}
+            </div>
+            <div className="col">
+              {" "}
+              <strong>Tracking #:</strong> <br /> {order.trackingId}{" "}
+            </div>
+          </div>
+        </div>
+        <div className="track">
+          <div
+            className={`step ${
+              order.status.includes("Order") ||
+              order.status.includes("Picked") ||
+              order.status.includes("On") ||
+              order.status.includes("Ready")
+                ? "active"
+                : null
+            }`}
+          >
+            {" "}
+            <span className="icon">
+              {" "}
+              <i className="fa fa-check"></i>{" "}
+            </span>{" "}
+            <span className="text">Order Confirmed</span>{" "}
+          </div>
+          <div
+            className={`step ${
+              order.status.includes("Picked") ||
+              order.status.includes("On") ||
+              order.status.includes("Ready")
+                ? "active"
+                : null
+            }`}
+          >
+            {" "}
+            <span className="icon">
+              {" "}
+              <i className="fa fa-user"></i>{" "}
+            </span>{" "}
+            <span className="text"> Picked By Courier</span>{" "}
+          </div>
+          <div
+            className={`step ${
+              order.status.includes("On") || order.status.includes("Ready")
+                ? "active"
+                : null
+            }`}
+          >
+            {" "}
+            <span className="icon">
+              {" "}
+              <i className="fa fa-truck"></i>{" "}
+            </span>{" "}
+            <span className="text"> On The Way </span>{" "}
+          </div>
+          <div
+            className={`step ${
+              order.status.includes("Ready") ? "active" : null
+            }`}
+          >
+            {" "}
+            <span className="icon">
+              {" "}
+              <i className="fa fa-box"></i>{" "}
+            </span>{" "}
+            <span className="text">Ready For Pickup</span>{" "}
+          </div>
+        </div>
 
-export default TrackingDetails
+        <ul className="row  py-5">
+          {order.items.map((orderItem, index) => (
+            <li className="col-md-4" key={index}>
+              <figure className="itemside mb-3">
+                <div className="aside">
+                  <img src={item} className="img-sm border" alt="" />
+                </div>
+                <figcaption className="info align-self-center">
+                  <p className="title">
+                    {orderItem.name} <br />{" "}
+                  </p>{" "}
+                  <span className="text-muted">${orderItem.price} </span>
+                </figcaption>
+              </figure>
+            </li>
+          ))}
+        </ul>
+
+        <h4 className="fw-bold">
+          {" "}
+          TOTAL: ${order.items.reduce((sum, item) => sum + item.price, 0)}{" "}
+        </h4>
+
+        <hr />
+        <Link
+          to="/"
+          className="btn btn-warning maroon-bg text-dark"
+          data-abc="true"
+        >
+          {" "}
+          <i className="fa fa-chevron-left"></i> Back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default TrackingDetails;
